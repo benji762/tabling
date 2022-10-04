8510972a173e85fac11d57a0b4174e89abc7d6b4
benji762 <112377860+benji762@users.noreply.github.com>
2022-10-04 12:32:57 +0900

    221004_1

--- js/slick_active.js
+++ js/slick_active.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$(".lazy").slick({
         lazyLoad: 'ondemand', // ondemand progressive anticipated
         infinite: true,
-		arrows : false,  
+		arrows : false,
+        autoplay : true,
       });
 });
---
+++ js/swiper_active.js
@@ -0,0 +1,17 @@
+new Swiper('.s2_slider', {
+	initialSlide: 0,
+	slidesPerView : 2,
+	spaceBetween : 16,
+	slidesPerGroup : 1,
+	// centeredSlides: true,
+	freeMode: true,
+});
+
+new Swiper('.s3_slider', {
+	initialSlide: 0,
+	slidesPerView : 3,
+	spaceBetween : 16,
+	slidesPerGroup : 1,
+	// centeredSlides: true,
+	freeMode: true,
+});
