85d34a06f1860b1f5c091596b9d23eb576040c3f
benji762 <112377860+benji762@users.noreply.github.com>
2022-10-14 11:54:46 +0900

    221014_1

--- js/click_hover.js
+++ js/click_hover.js
@@ -4,3 +4,13 @@ $(function(){
 		history.go(-1); // 방법1
 	})
 	})
+
+
+$(function(){
+	$('#section2 .btn_time').each(function(){
+		$(this).click(function(){
+			$('#section2 .btn_time').removeClass('on');
+			$(this).addClass('on');
+		})
+	})
+	})
