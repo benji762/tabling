80dd1a23d267e4e04737afc55a7f6ac109831347
benji762 <112377860+benji762@users.noreply.github.com>
2022-10-21 10:47:19 +0900

    221021

--- js/swiper_active.js
+++ js/swiper_active.js
@@ -1,35 +1,56 @@
 new Swiper('.s2_slider', {
 	initialSlide: 0,
-	slidesPerView : 2,
+	slidesPerView : 4,
 	spaceBetween : 16,
 	slidesPerGroup : 1,
 	// centeredSlides: true,
 	freeMode: true,
+	breakpoints: {
+		529: {
+			slidesPerView: 2,
+			spaceBetween: 16
+		  },
+		719: {
+			slidesPerView: 3,
+			spaceBetween: 32
+		 },
+	}
 });
 
 new Swiper('.s3_slider', {
 	initialSlide: 0,
-	slidesPerView : 3,
+	slidesPerView : 4,
 	spaceBetween : 16,
 	slidesPerGroup : 1,
 	// centeredSlides: true,
 	freeMode: true,
+	breakpoints: {
+		529: {
+			slidesPerView: 3,
+			spaceBetween: 16
+		  },
+		719: {
+			slidesPerView: 4,
+			spaceBetween: 32
+		 },
+	}
 });
 
 new Swiper('.search_s3_slider', {
 	initialSlide: 0,
-	slidesPerView : 2,
+	slidesPerView : 4,
 	spaceBetween : 16,
 	slidesPerGroup : 1,
 	// centeredSlides: true,
 	freeMode: true,
-});
-
-new Swiper('.review_s3_slider', {
-	initialSlide: 0,
-	slidesPerView : 2,
-	spaceBetween : 16,
-	slidesPerGroup : 1,
-	// centeredSlides: true,
-	freeMode: true,
+	breakpoints: {
+		529: {
+			slidesPerView: 2,
+			spaceBetween: 16
+		  },
+		719: {
+			slidesPerView: 3,
+			spaceBetween: 32
+		 },
+	}
 });
